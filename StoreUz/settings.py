35b5322742cf514d01bb6506f4089448c8e8e7a6
bnutss <b.nutss@mail.com>
2026-03-24 14:35:22 +0500

    fixed

--- StoreUz/settings.py
+++ StoreUz/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,6 +79,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
