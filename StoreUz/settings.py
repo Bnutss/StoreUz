e08d887d86f3d2591d82702367f9b7bb661f5a08
bnutss <b.nutss@mail.com>
2026-03-24 15:28:20 +0500

    fixed

--- StoreUz/settings.py
+++ StoreUz/settings.py
@@ -78,8 +78,9 @@ TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
